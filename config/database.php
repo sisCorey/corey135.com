c0c163bb6debbaf89694e44524d885ff7c233ef7
corey <zqcore@qq.com>
2018-01-23 21:36:36 +0800

    add ignore

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', '180.235.137.34'),
+            'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'corey135'),
-            'username'  => env('DB_USERNAME', 'corey'),
-            'password'  => env('DB_PASSWORD', 'dreams'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => 'c_',
